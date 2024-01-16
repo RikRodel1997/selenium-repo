2fd0a7ddfd4e1a31f7f36d8557fd4628e24c93fa
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-01-16 16:19:31 +0100

    Finished Rahul Shetty's Assignment 4

--- rahul_shetty_selenium_course/src/Assignment4.java
+++ rahul_shetty_selenium_course/src/Assignment4.java
@@ -1,3 +1,28 @@
-public class Assignment4 {
+import java.util.Set;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment4 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com");
+        
+        String originalWindowId = driver.getWindowHandle();
+
+        driver.findElement(By.linkText("Multiple Windows")).click();
+        driver.findElement(By.linkText("Click Here")).click();
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for (String window : allWindows) {
+            if (window != originalWindowId) {
+                driver.switchTo().window(window);
+            }
+        }
+        driver.switchTo().window(originalWindowId);
+        String openingANewWindowText = driver.findElement(By.xpath("//h3[text()='Opening a new window']")).getText();
+        assert openingANewWindowText.equals("Opening a new window");
+        System.out.println(openingANewWindowText);
+    }
 }
