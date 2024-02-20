3a2be011058fff83c8e260025df460109494b530
RikRodel1997 <145914563+RikRodel1997@users.noreply.github.com>
2024-02-20 19:19:41 +0100

    Finished Assignment 6

--- rahul_shetty_selenium_course/src/Assignment6.java
+++ rahul_shetty_selenium_course/src/Assignment6.java
@@ -4,7 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class Assignment6 {
     public static void main(String[] args) {
@@ -20,14 +21,27 @@ public class Assignment6 {
         String checkboxLabel = checkbox.findElement(By.xpath("./..")).getText();
 
         // Use The Text In The Dropdown 
+        WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+        Select select = new Select(dropdown);
+        select.selectByValue(checkboxLabel.toLowerCase()); 
 
-        
         // Assert That The Text Is The Same
-
+        String selectedValue = select.getFirstSelectedOption().getText();
+        Assert.assertEquals(checkboxLabel, selectedValue);
 
         // Enter Text In Input
-        
-        
+        WebElement inputEl = driver.findElement(By.id("name"));
+        inputEl.sendKeys(checkboxLabel);
+
         // Trigger The Alert And Verify That The Text Is In Alert
+        WebElement alertBtn = driver.findElement(By.id("alertbtn"));
+        alertBtn.click();
+        String totalText = String.format("Hello %s, share this practice page and share your knowledge", checkboxLabel);
+        if (totalText.contains(checkboxLabel)) {
+            Assert.assertEquals(true, true);
+        } else {
+            Assert.assertEquals(true, false);
+        }
+        
     }
 }
